42a9abd2b27b38d6fc47d2def9761c338cd3d447
Zhantorouulu_Kubat <78kuba00@gmail.com>
2022-11-04 11:29:01 +0600

    После начального комита, настроил settings, добавил index.html в папку с шаболнами и подключил во view

--- source/to_do_list/urls.py
+++ source/to_do_list/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import index_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_views)
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index_views(request):
+    return render(request, 'index.html')
